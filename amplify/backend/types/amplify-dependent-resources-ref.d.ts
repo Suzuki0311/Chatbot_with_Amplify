716880ce591a55b1d1d4d101377f79058dd3bd17
takeshi suzuki <takeshi.c.suzuki@gmail.com>
2023-03-25 16:23:11 +0900

    Amplify setup & gitignore make

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,23 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "chatGPTLineChatBotGraphQLApi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    },
+    "chatGPTLineChatBotRestApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "chatGPTLineChatBotFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}

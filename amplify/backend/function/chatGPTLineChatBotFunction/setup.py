00a726de719b4776540c7c15379a7b28beca11c0
takeshi suzuki <takeshi.c.suzuki@gmail.com>
2023-03-27 23:19:33 +0900

    setup.pyの修正 #3

--- amplify/backend/function/chatGPTLineChatBotFunction/setup.py
+++ amplify/backend/function/chatGPTLineChatBotFunction/setup.py
@@ -2,7 +2,7 @@
 
 # setup(name='src', version='1.0')
 
-# from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 from setuptools import setup, find_namespace_packages
 
 # setup(
@@ -11,9 +11,19 @@ from setuptools import setup, find_namespace_packages
 #     packages=find_packages(where='amplify/backend/function/chatGPTLineChatBotFunction/src'),
 #     package_dir={'': 'amplify/backend/function/chatGPTLineChatBotFunction/src'},
 # )
+# setup(
+#     # ...
+#     packages=find_namespace_packages(where='src'),
+#     package_dir={"": "src"}
+#     # ...
+# )
+
 setup(
     # ...
-    packages=find_namespace_packages(where='src'),
+    packages=find_packages(
+        where='src',
+        include=['my_package*'],  # alternatively: `exclude=['additional*']`
+    ),
     package_dir={"": "src"}
     # ...
 )

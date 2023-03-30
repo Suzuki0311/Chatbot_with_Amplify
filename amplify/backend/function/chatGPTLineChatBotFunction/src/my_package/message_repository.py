3446a2c4727a5d918a4383ab96944ee3dda56417
takeshi suzuki <takeshi.c.suzuki@gmail.com>
2023-03-31 02:40:41 +0900

    循環インポート回避のため、各種関数の引数修正

--- amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/message_repository.py
+++ amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/message_repository.py
@@ -7,13 +7,12 @@ from datetime import datetime
 from . import chatgpt_api
 from . import db_accessor
 from . import message_repository
-from . import index
 
 
 QUERY_LIMIT = 5
 
 
-def _fetch_chat_histories_by_line_user_id(line_user_id, prompt_text):
+def _fetch_chat_histories_by_line_user_id(line_user_id, prompt_text,message_image_id):
     try:
         if line_user_id is None:
             raise Exception('To query an element is none.')
@@ -30,7 +29,7 @@ def _fetch_chat_histories_by_line_user_id(line_user_id, prompt_text):
         chat_histories = list(map(lambda item: {"role": item["role"]["S"], "content": item["content"]["S"]}, reserved_results))
         print("chat_histories:",chat_histories)
         # Create the list of a current user prompt
-        if index.message_image_id != None:
+        if message_image_id != None:
             current_prompts = [{"role": "system", "content": "you are an excellent writer"},
                                {"role": "user", "content": prompt_text}]
         else:
@@ -62,13 +61,13 @@ def _insert_message(line_user_id, role, prompt_text):
         raise e
 
 
-def create_completed_text(line_user_id, prompt_text):
+def create_completed_text(line_user_id, prompt_text,message_image_id,text_language,user_language):
     # Query messages by Line user ID.
-    chat_histories = message_repository._fetch_chat_histories_by_line_user_id(line_user_id, prompt_text)
+    chat_histories = message_repository._fetch_chat_histories_by_line_user_id(line_user_id, prompt_text,message_image_id)
     print("chat_histories:",chat_histories)
 
     # Call the GPT3 API to get the completed text
-    completed_text = chatgpt_api.completions(chat_histories)
+    completed_text = chatgpt_api.completions(chat_histories,message_image_id,text_language,user_language)
     print("completed_text:",completed_text)
 
     # Put a record of the user into the Messages table.

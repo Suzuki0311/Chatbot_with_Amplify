281cd47656a3556277eaba41d313886e0ae54efc
takeshi suzuki <takeshi.c.suzuki@gmail.com>
2023-03-31 00:11:15 +0900

    画像があった場合にプロンプトを変更する処理を追加

--- amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/chatgpt_api.py
+++ amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/chatgpt_api.py
@@ -1,6 +1,7 @@
 import openai
 # import const
 from . import const
+from . import index
 
 # Model name
 GPT3_MODEL = 'gpt-3.5-turbo'
@@ -10,13 +11,16 @@ MAX_TOKENS = 1024
 
 # Create a new dict list of a system
 # SYSTEM_PROMPTS = [{'role': 'system', 'content': '敬語を使うのをやめてください。友達のようにタメ口で話してください。また、絵文字をたくさん使って話してください。'}]
-SYSTEM_PROMPTS = [{'role': 'system', 'content': 'Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.'}]
+if index.message_image_id != None:
+    SYSTEM_PROMPTS = [{"role": "user", "content": f"From this {index.text_language} text, identify important information such as an overview, items to be implemented, precautions, date and time, location, contact information, etc. And create a summary. If you don't have important information, don't reply about it. Finally reply in {index.user_language}."}]
+else:
+    SYSTEM_PROMPTS = [{'role': 'system', 'content': 'Please stop using polite language. Talk to me in a friendly way like a friend.'}]
 
 
 def completions(history_prompts):
     messages = SYSTEM_PROMPTS + history_prompts
 
-    print(f"prompts:{messages}")
+    print(f"全体_prompts:{messages}")
     try:
         openai.api_key = const.OPEN_AI_API_KEY
         response = openai.ChatCompletion.create(

72bc6f9ad6a2dd42ebbd62f24a4b724e47641850
takeshi suzuki <takeshi.c.suzuki@gmail.com>
2023-03-31 23:46:29 +0900

    日本語とポルトガル語のクイックリプライの実装

--- amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/index.py
+++ amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/index.py
@@ -14,11 +14,92 @@ from google.oauth2 import service_account
 from google.cloud import vision_v1p3beta1 as vision
 from google.cloud import translate_v2
 
-def create_quick_reply_buttons():
-    return [
-        QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
-        QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
-        QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+def create_quick_reply_buttons(user_language):
+    if user_language == 'Japanese':
+        return [
+            QuickReplyButton(action=MessageAction(label="日本語に翻訳", text="上記を日本語に翻訳してください")),
+            QuickReplyButton(action=MessageAction(label="英語に翻訳", text="上記を英語に翻訳してください")),
+            QuickReplyButton(action=MessageAction(label="1週間の献立", text="1週間の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="東京へ旅行", text="2泊3日で東京旅行の計画を立ててください")),
+            QuickReplyButton(action=MessageAction(label="読書感想文", text="ハリーポッター賢者の石の読書感想文を描いてください")),
+            QuickReplyButton(action=MessageAction(label="お金持ちになる", text="お金持ちになる方法を教えてください")),
+            QuickReplyButton(action=MessageAction(label="上司へお礼メール", text="飲み会でお世話になった上司へのお礼メールを書いてください")),
+            QuickReplyButton(action=MessageAction(label="日本の有名な観光名所", text="日本で有名な観光名所をいくつか教えてください")),
+            QuickReplyButton(action=MessageAction(label="詳しく", text="もっと詳しく教えてください")),
+            QuickReplyButton(action=MessageAction(label="英語学習", text="いくつかビジネスに必須な英単語とその　日本語訳をまとめたものを提示してください")),
+            QuickReplyButton(action=MessageAction(label="研究テーマ", text="私にいくつか質問して、大学の研究テーマを一緒に考えてください")),
+            QuickReplyButton(action=MessageAction(label="彼氏・彼女をなぐさめる", text="喧嘩した恋人と仲直りするためのメールを書いてください")),
+            QuickReplyButton(action=MessageAction(label="仕事疲れた", text="仕事疲れたので、私を元気付けてください"))
+    ]
+    elif user_language == 'English':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'Chinese':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'Spanish':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'French':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'German':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'Italian':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'Korean':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'Portuguese':
+        return  [
+            QuickReplyButton(action=MessageAction(label="Traduzir para Japonês", text="Traduza o acima para o japonês")),
+            QuickReplyButton(action=MessageAction(label="Traduzir para inglês", text="Traduza o acima para o inglês")),
+            QuickReplyButton(action=MessageAction(label="Cardápio de 1 semana", text="Sugira um menu para a semana")),
+            QuickReplyButton(action=MessageAction(label="Viajar para Tokyo", text="Planeje para min uma viagem para Tokyo por 3 dias e 2 noites")),
+            QuickReplyButton(action=MessageAction(label="Relatório de leitura", text="Desenhe um relatório de livro para a Pedra Filosofal de Harry Potter")),
+            QuickReplyButton(action=MessageAction(label="Ficar rico", text="Me diga como ficar rico")),
+            QuickReplyButton(action=MessageAction(label="E-mail de agradecimento ao chefe", text="Escreva um e-mail de agradecimento ao meu chefe que cuidou de min na festa")),
+            QuickReplyButton(action=MessageAction(label="Atração turística famosa do Japão", text="Diga-me algumas atrações turísticas famosas no Japão.")),
+            QuickReplyButton(action=MessageAction(label="Detalhe", text="Me conte mais em detalhes")),
+            QuickReplyButton(action=MessageAction(label="Aprender inglês", text="Por favor, apresente uma lista de algumas palavras essenciais em inglês para negócios e suas traduções para o português")),
+            QuickReplyButton(action=MessageAction(label="Tema de pesquisa", text="Por favor, faça-me algumas perguntas e crie um tópico de pesquisa para a universidade juntos.")),
+            QuickReplyButton(action=MessageAction(label="Conforte seu namorado/namorada", text="Escreva um e-mail para fazer as pazes com seu amante que brigou")),
+            QuickReplyButton(action=MessageAction(label="Cansei do trabalho", text="Estou cansado do trabalho, então, por favor, me anime"))
+    ]
+    elif user_language == 'Thai':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
+    ]
+    elif user_language == 'Vietnamese':
+        return [
+            QuickReplyButton(action=MessageAction(label="お問い合せ", text="お問い合せ")),
+            QuickReplyButton(action=MessageAction(label="今日の献立", text="今日の献立を提案してください")),
+            QuickReplyButton(action=MessageAction(label="上司へのお礼メール", text="上司へのお礼メールを書いてください"))
     ]
 
 def extract_info_from_event_body(event_body):
@@ -87,7 +168,7 @@ def handler(event, context):
         print("completed_text:",completed_text)
 
         # Create quick reply buttons
-        quick_reply_buttons = create_quick_reply_buttons()
+        quick_reply_buttons = create_quick_reply_buttons(user_language)
         quick_reply = QuickReply(items=quick_reply_buttons)
 
         # Reply the message using the LineBotApi instance with quick replies

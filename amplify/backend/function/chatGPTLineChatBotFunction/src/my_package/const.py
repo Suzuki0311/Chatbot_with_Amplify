3ebc8579001d3c48cb9b63a5f25f37b80c928212
takeshi suzuki <takeshi.c.suzuki@gmail.com>
2023-04-03 22:50:43 +0900

    ローカルサーバー用の環境変数の設定 #2

--- amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/const.py
+++ amplify/backend/function/chatGPTLineChatBotFunction/src/my_package/const.py
@@ -2,9 +2,18 @@ import os
 # import aws_systems_manager
 from . import aws_systems_manager
 
-BASE_SECRET_PATH = os.environ.get('BASE_SECRET_PATH')
-DB_TABLE_NAME_POSTFIX = os.environ.get('DB_TABLE_NAME_POSTFIX')
-OPEN_AI_API_KEY = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}OPEN_AI_API_KEY')
-LINE_CHANNEL_SECRET = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}LINE_CHANNEL_SECRET')
-LINE_CHANNEL_ACCESS_TOKEN = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}LINE_CHANNEL_ACCESS_TOKEN')
-GOOGLE_APPLICATION_CREDENTIALS = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}GOOGLE_APPLICATION_CREDENTIALS')
+
+is_local = os.environ.get('IS_LOCAL', 'False') == 'True'
+
+if is_local:
+    OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
+    LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
+    LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+    GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+else:
+    BASE_SECRET_PATH = os.environ.get('BASE_SECRET_PATH')
+    DB_TABLE_NAME_POSTFIX = os.environ.get('DB_TABLE_NAME_POSTFIX')
+    OPEN_AI_API_KEY = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}OPEN_AI_API_KEY')
+    LINE_CHANNEL_SECRET = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}LINE_CHANNEL_SECRET')
+    LINE_CHANNEL_ACCESS_TOKEN = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}LINE_CHANNEL_ACCESS_TOKEN')
+    GOOGLE_APPLICATION_CREDENTIALS = aws_systems_manager.get_secret(f'{BASE_SECRET_PATH}GOOGLE_APPLICATION_CREDENTIALS')

c3b9f8dd82c82fe084afdb1ef53f61e39a401e3a
takeshi suzuki <takeshi.c.suzuki@gmail.com>
2023-04-24 22:07:19 +0900

    db_accessor.pyの関数実装とrequirements.txtの更新

--- amplify/backend/function/stripeWebhookHandler/src/index.py
+++ amplify/backend/function/stripeWebhookHandler/src/index.py
@@ -1,11 +1,17 @@
 import json
-import const
-import aws_systems_manager
+from . import aws_systems_manager
+from . import const
 
 
 def handler(event, context):
   print('received event:')
   print(event)
+
+  if event['type'] == 'checkout.session.completed':
+    session = event['data']['object']
+    line_user_id = session['client_reference_id']
+    customer_id = session['customer']
+
   
   return {
       'statusCode': 200,
